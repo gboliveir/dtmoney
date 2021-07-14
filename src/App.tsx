7e22c67d041934177b3c3241b0d18b54e995f204
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-07-14 19:25:27 -0300

    Criaçao e estilizaçao de componente Dashboard e Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-      <GlobalStyle />
       <Header />
+      <Dashboard />
+      <GlobalStyle />
     </div>
   );
 }
