b61f1f9e1febb267841c83d884c7c880d832af73
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-07-14 20:24:12 -0300

    Criaçao e estilizaçao de componente TrasactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 
 import { Summary } from "../Summary";
+import { TransanctionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
     return(
         <Container>
             <Summary />
+            <TransanctionsTable />
         </Container>
     );
 }
