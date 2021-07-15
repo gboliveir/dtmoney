d5ce3c6bb69fe13c3b1fbb03e2040b464a6e70c1
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-07-15 14:15:53 -0300

    Configurando estados de abertura e fechamento do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import Modal from 'react-modal';
+
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      
       <Dashboard />
+
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Nova transação</h2>
+      </Modal>
       <GlobalStyle />
     </div>
   );
