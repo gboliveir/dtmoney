0f2304aad2628f27a4c3b7ab26cc880ea0c34b79
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-07-15 16:54:58 -0300

    Adicionada algumas estilizaçoes e esqueleto ao React Modal

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionsModalProps {
     isOpen: boolean,
@@ -10,8 +11,20 @@ export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactions
         <Modal 
             isOpen={isOpen} 
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Nova transação</h2>
+            <Container>
+                <h2>Nova transação</h2>
+                
+                <input placeholder="Título" type="text" />
+                <input placeholder="Valor" type="number" />
+                <input placeholder="Categoria" type="text" />
+                
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     );
 }
